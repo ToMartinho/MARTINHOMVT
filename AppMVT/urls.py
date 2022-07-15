82e210fa6ef859bd688a7eb2288721449d55cbd4
ToMartinho <tome.martinho@gmail.com>
2022-07-15 11:27:23 -0300

    se agrego funcion agrega_familiares y su respectivo url

--- AppMVT/urls.py
+++ AppMVT/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from AppMVT.views import agrega_familiar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agrega-familiar/', agrega_familiar),
 ]
--- AppMVT/views.py
+++ AppMVT/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from AppMVT.models import Familiares
+
 # Create your views here.
+def agrega_familiar(self, tipo_f, nombre, apellido, edad, estado_f):
+
+    familiar = Familiares(tipo_f=tipo_f, nombre=nombre, apellido=apellido, edad=edad, estado_f=estado_f)
+    familiar.save()
+
+    return HttpResponse(f"""
+            <p>El familiar tipo: {familiar.tipo_f} con nombre y apellido: {familiar.nombre} {familiar.apellido} edad: {familiar.edad} y estado: {familiar.estado_f} se agrego a la DB</p>
+    """)
