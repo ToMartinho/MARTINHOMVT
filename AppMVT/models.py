fffe8cc6e300edfe617ff9710965d6264b1b1fee
ToMartinho <tome.martinho@gmail.com>
2022-07-15 10:59:31 -0300

    se modifica carpeta

--- AppMVT/models.py
+++ AppMVT/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Familiar(models.Model):
+    tipo_f = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
