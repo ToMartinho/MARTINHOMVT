6381e66d35f1783fa6eabed09bb121fd49200ee1
ToMartinho <tome.martinho@gmail.com>
2022-07-15 11:50:58 -0300

    se agrego a traves del url agrega-familiar 4 familiares a la BD

--- AppMVT/urls.py
+++ AppMVT/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from AppMVT.views import agrega_familiar
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('agrega-familiar/<tipo_f>/<nombre>/<apellido>/<edad>/<estado>', agrega_familiar),
+        path('agrega-familiar/<tipo_f>/<nombre>/<apellido>/<edad>/<estado_f>/', agrega_familiar),
 ]
