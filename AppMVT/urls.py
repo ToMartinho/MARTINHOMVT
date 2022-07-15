f873e6a2268f8152aee85160e09fdd512761dc92
ToMartinho <tome.martinho@gmail.com>
2022-07-15 12:11:40 -0300

    se agrego funcionabiliadad de lsita al template de familiares y url correspondiente

--- AppMVT/urls.py
+++ AppMVT/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from AppMVT.views import agrega_familiar
+from AppMVT.views import agrega_familiar , lista_familiares
 
 urlpatterns = [
         path('agrega-familiar/<tipo_f>/<nombre>/<apellido>/<edad>/<estado_f>/', agrega_familiar),
+        path('lista-familiares/', lista_familiares),
 ]
